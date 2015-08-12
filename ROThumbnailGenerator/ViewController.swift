43401939c4b7181970fc099bd18317c2d4ea75ff
Robin Oster <robin.oster@rascor.com>
2015-08-12 10:30:40 +0200

    Initial Commit

---
+++ ROThumbnailGenerator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ROThumbnailGenerator
+//
+//  Created by Robin Oster on 12/08/15.
+//  Copyright (c) 2015 Prine Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
